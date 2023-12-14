6abf938954abef940fad4fc43cac0b1f9f871e20
fckvania <vaniaalexanakano@gmail.com>
2023-12-14 09:40:57 +0700

    bonus

--- src/commands/main-sc.go
+++ src/commands/main-sc.go
@@ -4,7 +4,7 @@ import "mao/src/libs"
 
 func init() {
 	libs.NewCommands(&libs.ICommand{
-		Name:     "ping",
+		Name:     "sc",
 		Tags:     "main",
 		IsPrefix: true,
 		Exec: func(client *libs.NewClientImpl, m *libs.IMessage) {
