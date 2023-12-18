53b8910e73eae9cb5c96f7bbc8c53651b558f4b2
fckvania <vaniaalexanakano@gmail.com>
2023-12-18 18:24:33 +0700

    fixed & add fitur

--- src/commands/download-yt.go
+++ src/commands/download-yt.go
@@ -40,8 +40,9 @@ func init() {
 				for i, v := range ser {
 					D, _ := v.GetDuration()
 					if D < 8*time.Minute {
-						ser = append(ser[:i], ser[i+1:]...)
 						ser = append(ser, v)
+					} else {
+						ser = append(ser[:i], ser[i+1:]...)
 					}
 				}
 				url = ser[rand.Intn(len(ser))].URL
